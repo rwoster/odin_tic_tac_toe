07977b8341ac0fa41a91280d1985b90b7c4d4cdf
rwoster <>
2022-11-04 13:18:45 +0000

    added tests to game_spec and fixed typo in board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,19 +1,19 @@
-require_relative "../board.rb"
+require_relative '../board.rb'
 
 describe Board do
   subject(:board) { described_class.new }
-  
-  describe "#initialize" do
-    it "returns @cells" do
-      expect(board.cells).to eql([1,2,3,4,5,6,7,8,9])
+
+  describe '#initialize' do
+    it 'returns @cells' do
+      expect(board.cells).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
-  describe "#show_board" do
+  describe '#show_board' do
     # no test needed, just puts the board
   end
 
-  describe "#update_board" do
+  describe '#update_board' do
     it "updates cell 3 with symbol 'X'" do
       number = 3
       symbol = 'X'
@@ -31,24 +31,24 @@ describe Board do
     end
   end
 
-  describe "#valid_move?" do
-    context "when the move is valid" do
-      it "returns true" do
+  describe '#valid_move?' do
+    context 'when the move is valid' do
+      it 'returns true' do
         valid_entry = 3
         turn = board.valid_move?(valid_entry)
         expect(turn).to be true
       end
     end
 
-    context "when the move is invalid" do
-      it "returns false for out of range move" do 
+    context 'when the move is invalid' do
+      it 'returns false for out of range move' do
         invalid_entry = 42
         turn = board.valid_move?(invalid_entry)
         expect(turn).to be false
       end
-      
-      it "returns false for attempt to move on occupied space" do
-        board.instance_variable_set(:@cells, [1,2,'X',4,5,6,7,8,9])
+
+      it 'returns false for attempt to move on occupied space' do
+        board.instance_variable_set(:@cells, [1, 2, 'X', 4, 5, 6, 7, 8, 9])
         occupied_cell = 3
         turn = board.valid_move?(occupied_cell)
         expect(turn).to be false
@@ -56,75 +56,73 @@ describe Board do
     end
   end
 
-  describe "#full?" do
-    context "when the board is not full (new board)" do
-      it "returns false" do
+  describe '#full?' do
+    context 'when the board is not full (new board)' do
+      it 'returns false' do
         expect(board).not_to be_full
       end
     end
-    
-    context "when the board is mid game" do
+
+    context 'when the board is mid game' do
       before do
         board.instance_variable_set(:@cells, [1, 'x', 3, 'o', 5, 'x', 'x', 8, 'o'])
       end
-      
-      it "returns false" do
+
+      it 'returns false' do
         expect(board).not_to be_full
       end
     end
-    
-    context "when the board is full" do
+
+    context 'when the board is full' do
       before do
         board.instance_variable_set(:@cells, %w[x o x x o x o x o])
       end
-      
-      it "returns true" do
+
+      it 'returns true' do
         expect(board).to be_full
       end
     end
   end
 
-  describe "#game_over?" do
-    context "when the game is not over" do
+  describe '#game_over?' do
+    context 'when the game is not over' do
       before do
-        board.instance_variable_set(:@cells, [1,2,3,"X",5,"O",7,8,9]) 
+        board.instance_variable_set(:@cells, [1, 2, 3, 'X', 5, 'O', 7, 8, 9])
       end
 
-      it "returns false" do
-        expect(board).not_to be_game_over  
+      it 'returns false' do
+        expect(board).not_to be_game_over
       end
     end
 
-    context "when the game is over, with vertical win" do
+    context 'when the game is over, with vertical win' do
       before do
-        board.instance_variable_set(:@cells, ["X",2,3,"X",5,6,"X",8,9]) 
+        board.instance_variable_set(:@cells, ['X', 2, 3, 'X', 5, 6, 'X', 8, 9])
       end
 
-      it "returns true" do
-        expect(board).to be_game_over  
+      it 'returns true' do
+        expect(board).to be_game_over
       end
     end
 
-    context "when the game is over, with horizontal win" do
+    context 'when the game is over, with horizontal win' do
       before do
-        board.instance_variable_set(:@cells, ["O","O","O",4,5,6,7,8,9]) 
+        board.instance_variable_set(:@cells, ['O', 'O', 'O', 4, 5, 6, 7, 8, 9])
       end
 
-      it "returns true" do
-        expect(board).to be_game_over  
+      it 'returns true' do
+        expect(board).to be_game_over
       end
     end
 
-    context "when the game is over, with diagonal win" do
+    context 'when the game is over, with diagonal win' do
       before do
-        board.instance_variable_set(:@cells, ["X",2,3,4,"X",6,7,8,"X"]) 
+        board.instance_variable_set(:@cells, ['X', 2, 3, 4, 'X', 6, 7, 8, 'X'])
       end
 
-      it "returns true" do
-        expect(board).to be_game_over  
+      it 'returns true' do
+        expect(board).to be_game_over
       end
     end
-    
   end
-
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,52 @@
 require_relative "../game.rb"
+require_relative "../board.rb"
+require_relative "../player.rb"
 
 describe Game do
 
+  subject(:game) { described_class.new }
+
+  before do
+    game.instance_variable_set(:@board, instance_double(Board))
+  end
   
+  describe "#play" do
+    it "runs main game loop" do
+      allow(game).to receive(:game_set_up)
+      allow(game).to receive(:player_turns)
+      allow(game).to receive(:conclusion)
+      expect(game.board).to receive(:show_board)
+      game.play
+    end
+  end
+
+  describe "#create_player" do
+    it "creates a player" do
+      name = 'Satoshi'
+      symbol = "₿"
+      allow(game).to receive(:puts)
+      allow(game).to receive(:text_player_name).with(1)
+      allow(game).to receive(:gets).and_return(name)
+      allow(game).to receive(:symbol_input).and_return(symbol)
+      expect(Player).to receive(:new).with(name, symbol)
+      game.create_player(1)
+    end
+  end
+
+  describe "#turn" do
+    let(:symbol) { "₿" }
+    let(:turn) { 4 }
+    let(:mock_player) { instance_double(Player, :symbol => symbol) }
+    
+    before do
+      game.instance_variable_set(:@first_player, mock_player)
+    end    
+    
+    it "runs turn loop" do
+      allow(game).to receive(:turn_input).with(game.first_player).and_return(turn)
+      allow(game.board).to receive(:show_board)
+      expect(game.board).to receive(:update_board).with(turn - 1, symbol)
+      game.turn(game.first_player)
+    end
+  end
 end
